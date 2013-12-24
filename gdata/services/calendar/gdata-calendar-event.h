6140eb9b2e4b1a50ab579094da6613fb009fc345
william7777 <will.yu@aol.com>
2013-12-23 20:48:10 -0500

    Update gdata-calendar-event.h

--- gdata/services/calendar/gdata-calendar-event.h
+++ gdata/services/calendar/gdata-calendar-event.h
@@ -38,23 +38,22 @@ G_BEGIN_DECLS
 #define GDATA_IS_CALENDAR_EVENT_CLASS(k)	(G_TYPE_CHECK_CLASS_TYPE ((k), GDATA_TYPE_CALENDAR_EVENT))
 #define GDATA_CALENDAR_EVENT_GET_CLASS(o)	(G_TYPE_INSTANCE_GET_CLASS ((o), GDATA_TYPE_CALENDAR_EVENT, GDataCalendarEventClass))
 
-//newly added
-#define GDATA_CALENDAR_EVENT_VISIBILITY_DEFAULT "defaulst"
-#define GDATA_CALENDAR_EVENT_VISIBILITY_PUBLIC  "public"
-#define GDATA_CALENDAR_EVENT_VISIBILITY_PRIVATE "private"
-#define GDATA_CALENDAR_EVENT_VISIBILITY_CONFIDENTIAL "confidential"
+#define GDATA_CALENDAR_EVENT_VISIBILITY_DEFAULT         "default"
+#define GDATA_CALENDAR_EVENT_VISIBILITY_PUBLIC          "public"
+#define GDATA_CALENDAR_EVENT_VISIBILITY_PRIVATE         "private"
+#define GDATA_CALENDAR_EVENT_VISIBILITY_CONFIDENTIAL    "confidential"
 
-#define GDATA_CALENDAR_EVENT_TRANSPARENCY_OPAQUE "opaque"
+#define GDATA_CALENDAR_EVENT_TRANSPARENCY_OPAQUE        "opaque"
 #define GDATA_CALENDAR_EVENT_TRANSPARENCY_TRANSPARENT   "transparent"
 
-#define GDATA_CALENDAR_EVENT_STATUS_CONFIRMED   "confirmed"
-#define GDATA_CALENDAR_EVENT_STATUS_TENTATIVE   "tentative"
-#define GDATA_CALENDAR_EVENT_STATUS_CANCELLED   "cancelled"
+#define GDATA_CALENDAR_EVENT_STATUS_CONFIRMED           "confirmed"
+#define GDATA_CALENDAR_EVENT_STATUS_TENTATIVE           "tentative"
+#define GDATA_CALENDAR_EVENT_STATUS_CANCELLED           "cancelled"
 
 #define GDATA_CALENDAR_EVENT_RESPONSE_STATUS_NEEDSACTION        "needsAction";
-#define GDATA_CALENDAR_EVENT_RESPONSE_STATUS_DECLINED   "declined"
-#define GDATA_CALENDAR_EVENT_RESPONSE_STATUS_TENTATIVE  "tentative"
-#define GDATA_CALENDAR_EVENT_RESPONSE_STATUS_ACCEPTED   "accepted"
+#define GDATA_CALENDAR_EVENT_RESPONSE_STATUS_DECLINED           "declined"
+#define GDATA_CALENDAR_EVENT_RESPONSE_STATUS_TENTATIVE          "tentative"
+#define GDATA_CALENDAR_EVENT_RESPONSE_STATUS_ACCEPTED           "accepted"
 
 typedef struct _GDataCalendarEventPrivate	GDataCalendarEventPrivate;
 
@@ -112,12 +111,45 @@ const gchar *gdata_calendar_event_get_recurrence (GDataCalendarEvent *self) G_GN
 void gdata_calendar_event_set_recurrence (GDataCalendarEvent *self, const gchar *recurrence);
 void gdata_calendar_event_get_original_event_details (GDataCalendarEvent *self, gchar **event_id, gchar **event_uri);
 gboolean gdata_calendar_event_is_exception (GDataCalendarEvent *self) G_GNUC_PURE;
-
-const gchar* gdata_calendar_event_get_description(GDataCalendarEvent *self) G_GNUC_PURE;
-void gdata_calendar_event_set_description(GDataCalendarEvent *self, const gchar* description);
-
-const char* gdata_calendear_event_get_calendar_id (GDataCalendarEvent *self) G_GNUC_PURE;
-void gdata_calendar_event_set_color_id(GDataCalendarEvent *self, const char* color_id);
+const gchar* gdata_calendar_event_get_description (GDataCalendarEvent *self) G_GNUC_PURE;
+void gdata_calendar_event_set_description (GDataCalendarEvent *self, const gchar *description);
+void gdata_calendar_event_get_color (GDataCalendarEvent *self, GDataColor *color);
+void gdata_calendar_event_set_color (GDataCalendarEvent *self, const GDataColor *color);
+gboolean gdata_calendar_event_is_attendees_omitted (GDataCalendarEvent *self) G_GNUC_PURE;
+void gdata_calendar_event_set_is_attendees_omitted (GDataCalendarEvent *self, gboolean is_attendees_omitted);
+gboolean gdata_calendar_event_is_private_copy (GDataCalendarEvent *self) G_GNUC_PURE;
+gboolean gdata_calendar_event_is_locked (GDataCalendarEvent *self) G_GNUC_PURE;
+const gchar *gdata_calendar_event_get_summary (GDataCalendarEvent *self) G_GNUC_PURE;
+void gdata_calendar_event_set_summary (GDataCalendarEvent *self, const gchar *summary);
+const gchar *gdata_calendar_event_get_hangout_link (GDataCalendarEvent *self) G_GNUC_PURE;
+gboolean gdata_calendar_event_is_end_time_unspecified (GDataCalendarEvent *self) G_GNUC_PURE;
+const gchar *gdata_calendar_event_get_extended_property_private (GDataCalendarEvent *self, const gchar *name) G_GNUC_PURE;
+GHashTable *gdata_calendar_event_get_extended_properties_private (GDataCalendarEvent *self) G_GNUC_PURE;
+gboolean gdata_calendar_event_set_extended_property_private (GDataCalendarEvent *self, const gchar *name, const gchar *value);
+const gchar *gdata_calendar_event_get_extended_property_shared (GDataCalendarEvent *self, const gchar *name) G_GNUC_PURE;
+GHashTable *gdata_calendar_event_get_extended_properties_shared (GDataCalendarEvent *self) G_GNUC_PURE;
+gboolean gdata_calendar_event_set_extended_property_shared (GDataCalendarEvent *self, const gchar *name, const gchar *value);
+const gchar* gdata_calendar_event_get_source_title (GDataCalendarEvent *self) G_GNUC_PURE;
+void gdata_calendar_event_set_source_title (GDataCalendarEvent *self, const gchar* source_title);
+const gchar* gdata_calendar_event_get_source_url (GDataCalendarEvent *self) G_GNUC_PURE;
+void gdata_calendar_event_set_source_url (GDataCalendarEvent *self, const gchar* source_url);
+const gchar* gdata_calendar_event_get_gadget_display (GDataCalendarEvent *self) G_GNUC_PURE;
+void gdata_calendar_event_set_gadget_display (GDataCalendarEvent *self, const gchar* gadget_display);
+const gchar* gdata_calendar_event_get_gadget_icon_link (GDataCalendarEvent *self) G_GNUC_PURE;
+void gdata_calendar_event_set_gadget_icon_link (GDataCalendarEvent *self, const gchar* gadget_icon_link);
+const gchar* gdata_calendar_event_get_gadget_link (GDataCalendarEvent *self) G_GNUC_PURE;
+void gdata_calendar_event_set_gadget_link (GDataCalendarEvent *self, const gchar* gadget_link);
+const gchar* gdata_calendar_event_get_gadget_title (GDataCalendarEvent *self) G_GNUC_PURE;
+void gdata_calendar_event_set_gadget_title (GDataCalendarEvent *self, const gchar* gadget_title);
+const gchar* gdata_calendar_event_get_gadget_type (GDataCalendarEvent *self) G_GNUC_PURE;
+void gdata_calendar_event_set_gadget_type (GDataCalendarEvent *self, const gchar* gadget_type);
+guint gdata_calendar_event_get_gadget_height (GDataCalendarEvent *self) G_GNUC_PURE;
+void gdata_calendar_event_set_gadget_height (GDataCalendarEvent *self, guint gadget_height);
+guint gdata_calendar_event_get_gadget_width (GDataCalendarEvent *self) G_GNUC_PURE;
+void gdata_calendar_event_set_gadget_width (GDataCalendarEvent *self, guint gadget_width);
+const gchar *gdata_calendar_event_get_gadget_preference (GDataCalendarEvent *self, const gchar *name) G_GNUC_PURE;
+GHashTable *gdata_calendar_event_get_gadget_preferences (GDataCalendarEvent *self) G_GNUC_PURE;
+gboolean gdata_calendar_event_set_gadget_preference (GDataCalendarEvent *self, const gchar *name, const gchar *value);
 
 G_END_DECLS
 
